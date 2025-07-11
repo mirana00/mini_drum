514bda679e0def760c9b20cfa02d7af343a452db
mirana00 <miranahanif@gmail.com>
2025-07-11 15:39:06 +0600

    added css and made modile friendly

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const sounds = {
     F: "sounds/ride.mp3",
     G: "sounds/snare.mp3",
     H: "sounds/tom.mp3",
+    V: "sounds/crashCymbal.mp3",
+
 
 };
 
@@ -17,6 +19,33 @@ if(soundFile) {
     const audio = new Audio(soundFile);
     audio.play();
 
-}
+    const button = document.querySelector(`button[data-key="${key}"]`);
+    if (button) {
+      button.classList.add("active");
 
+      // Remove the class after the animation finishes
+      setTimeout(() => {
+        button.classList.remove("active");
+      }, 200);
+}
+}
 });
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("touchstart", () => {
+    const key = btn.dataset.key;
+    const soundFile = sounds[key];
+
+    if (soundFile) {
+      const audio = new Audio(soundFile);
+      audio.play();
+
+      btn.classList.add("active");
+      setTimeout(() => btn.classList.remove("active"), 200);
+    }
+  });
+});
+
+
